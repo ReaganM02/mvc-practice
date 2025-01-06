30ac4dee2558f324e250d37d0a1799877c0e0f95
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-07 03:02:18 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,8 +6,32 @@ function prettyArray($array) {
   echo '</pre>';
 }
 
-function abort($statusCode = 404) {
+function abort($statusCode = Response::NOT_FOUND) {
   http_response_code($statusCode);
   require_once 'views/' . $statusCode . '.php';
   die();
 }
+
+/**
+ * Use to load a view file
+ * @param string $path - file path
+ * @return void
+ */
+function loadTemplate($path, $data=[]) {
+  extract($data);
+  require_once __DIR__ . $path;
+}
+
+function fieldErrorClass($fieldName, $classes=[]) {
+  if(empty($fieldName)) {
+    return implode($classes);
+  }
+  return '';
+}
+
+function fieldErrorMsg($fieldName) {
+  if(empty($fieldName)) {
+    return $fieldName;
+  }
+  return '';
+} 
