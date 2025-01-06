a82d6c29d479a5e246193bc32bfd13cf0ba788ad
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-07 03:35:30 +0800

    Added post model file

--- models/post.model.php
+++ models/post.model.php
@@ -9,6 +9,19 @@ class PostModel extends Database {
 
     $this->connection = $this->connect($this->config['database'], $this->config['user'], $this->config['password']);
   }
+
+  public function createPost($title, $body, $userID) {
+    $title = $_POST['post-title'];
+    $statement = $this->connection->prepare('INSERT INTO posts(title, user_id, content) VALUES(:title, :user_id, :content)');
+    $statement->bindValue(':title', $title, PDO::PARAM_STR);
+    $statement->bindValue(':content', $body, PDO::PARAM_STR);
+    $statement->bindValue(':user_id', $userID, PDO::PARAM_INT);
+
+    if($statement->execute()) {
+      return true;
+    }
+    return false;
+  }
   
   public function posts() {
     $query = "select * from posts";
@@ -24,13 +37,50 @@ class PostModel extends Database {
   }
 
   public function getPostsByUserID($id) {
-    $query = "select * from posts where user_id = {$id}";
     
-    $statement = $this->connection->prepare($query);
+    $statement = $this->connection->prepare("select * from posts where user_id = :id");
+
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+
     $statement->execute();
 
     $posts = $statement->fetchAll();
 
     return $posts;
+  }
+
+  public function getPost($id) {
+
+    $statement = $this->connection->prepare("select * from posts where id = :id");
+
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $post = $statement->fetch();
+
+    return $post;
+  }
+
+  /**
+   * Get post by user ID
+   * @param int $postID
+   * @param int $userID
+   * @return array {title: string, id: int, user_id: int}|false The post data or false if not found.
+   */
+  public function getPostByUserID($postID, $userID) {
+    $statement = $this->connection->prepare("select * from posts where id = :id and user_id = :userID");
+
+    $statement->bindValue(':id', $postID, PDO::PARAM_INT);
+    $statement->bindValue(':userID', $userID, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $post = $statement->fetch();
+
+    if(!$post) {
+      return false;
+    }
+    return $post;
   }
 }
